cdf5f0444490ae9ce764a7b3fdea215a040132cf
Syed Ali <sali3@cox.net>
2016-11-01 15:29:13 -0400

    implemented Ajax. Tryig to put data in array

--- js/data.js
+++ js/data.js
@@ -1,6 +1,8 @@
 (function(){
 "use strict";
 
+window.github = window.github || {};
+
 
 console.log("I am in data.js file");
 
--- js/ui.js
+++ js/ui.js
@@ -1,20 +1,35 @@
 (function(){
 "use strict";
 
-console.log("I am in ui.js file");
+window.github = window.github || {};
+
+window.github.storage = [];
 
 
 
+$("button")
+  .on("click", function getData(){
+    var token = $("input").val();
+    $.ajax({
+      url:"https://api.github.com/users/jisaacks/orgs",
+      method: "GET",
+      dataType: "json",
+      headers: {
+      Authorization: "token " + token
+      }
 
+    })
+    .done(function processData(incomingData){
+    console.log("Received data from git", incomingData);
+    })
 
+    .fail(function noData(){
+    console.log("Did not get data");
+  });
 
+    // storage.push[incomingData];
 
-
-
-
-
-
-
+});
 
 
 })();
