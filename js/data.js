258dd39e4b3aba9a1929034ec00afd8a2ce76e28
Syed Ali <sali3@cox.net>
2016-11-02 05:19:59 -0400

    Completed CSS styling of list, refactored and scrubbed code by using jshint

--- js/data.js
+++ js/data.js
@@ -6,37 +6,25 @@ window.github = window.github || {};
 
 $("button")
   .on("click", function getData(){
-    var token = $("input").val();
-    $.ajax({
-      url:"https://api.github.com/users/jisaacks/orgs",
-      method: "GET",
-      dataType: "json",
-      headers: {
-      Authorization: "token " + token
-      }
 
+      var token = $("input").val();
 
+      $.ajax({
+        url:"https://api.github.com/users/jisaacks/orgs",
+        method: "GET",
+        dataType: "json",
+        headers: {
+          Authorization: "token " + token
+        }
+      })
 
-    })
+      .done(function processData(ajaxIncomingData){
+        window.github.buildList(ajaxIncomingData);
+      })
 
-    .done(function processData(ajaxIncomingData){
-      // console.log("Received data from git", ajaxIncomingData);
-
-      console.log(ajaxIncomingData);
-
-      window.github.buildList(ajaxIncomingData)
-
-    })
-
-    .fail(function noData(){
-      console.log("Did not get data");
-    });
-
-
-
-// console.log(storage);
-
-
+      .fail(function noData(){
+        console.log("Did not get data");
+      });
 
   });
 
