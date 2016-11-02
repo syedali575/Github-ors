31ee638dcefcf884fe1dcb8758e3e8cd484d87f5
Syed Ali <sali3@cox.net>
2016-11-01 22:08:42 -0400

    Got images and data in right list, some more style to be done

--- js/ui.js
+++ js/ui.js
@@ -1,24 +1,17 @@
 (function() {
+
 "use strict";
+
 
 window.github = window.github || {};
 
 window.github.buildList = function buildList(data) {
-  console.log("buildList", data);
 
-$.each(data, function loopArray(i){
-  // console.log(i);
-$(".list").append('<li><img src="' + data[i].avatar_url + '"></img><p>' + data[i].login + '/p></li>');
-});
+    $.each(data, function loopArray(i){
+        $(".list").append('<li><img src="'+data[i].avatar_url+'"></img><p>' + data[i].login + '</p></li>');
+    });
 
 };
 
 
-
-
 })();
-
-
-//   $(data).each( function processData(i){
-//
-// //   });
